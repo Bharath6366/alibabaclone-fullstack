5b3602e53ee79b1a55e26abff79fdc871b81db33
Bharath6366 <bb94834045@gmail.com>
2026-05-10 11:06:44 +0530

    Connected frontend to live backend

--- server/server.js
+++ server/server.js
@@ -1,15 +1,24 @@
 require("dotenv").config();
 
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
+const express =
+  require("express");
+const mongoose =
+  require("mongoose");
+const cors =
+  require("cors");
 
-const authRoutes = require("./routes/auth");
-const adminRoutes = require("./routes/admin");
-const productRoutes = require("./routes/product");
-const analyticsRoutes = require("./routes/analytics");
-const paymentRoutes = require("./routes/payment");
-const orderRoutes = require("./routes/orders");
+const authRoutes =
+  require("./routes/auth");
+const adminRoutes =
+  require("./routes/admin");
+const productRoutes =
+  require("./routes/product");
+const analyticsRoutes =
+  require("./routes/analytics");
+const paymentRoutes =
+  require("./routes/payment");
+const orderRoutes =
+  require("./routes/orders");
 
 const app = express();
 
@@ -17,9 +26,13 @@ app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(
+    process.env.MONGO_URI
+  )
   .then(() =>
-    console.log("MongoDB Connected")
+    console.log(
+      "MongoDB Connected"
+    )
   )
   .catch((err) =>
     console.log(
@@ -28,17 +41,47 @@ mongoose
     )
   );
 
-app.use("/api/auth", authRoutes);
-app.use("/api/admin", adminRoutes);
-app.use("/api/products", productRoutes);
-app.use("/api/analytics", analyticsRoutes);
-app.use("/api/payment", paymentRoutes);
-app.use("/api/orders", orderRoutes);
+app.use(
+  "/api/auth",
+  authRoutes
+);
+
+app.use(
+  "/api/admin",
+  adminRoutes
+);
+
+app.use(
+  "/api/products",
+  productRoutes
+);
+
+app.use(
+  "/api/analytics",
+  analyticsRoutes
+);
+
+app.use(
+  "/api/payment",
+  paymentRoutes
+);
+
+app.use(
+  "/api/orders",
+  orderRoutes
+);
 
 app.get("/", (req, res) => {
-  res.send("Backend Running");
+  res.send(
+    "Backend Running"
+  );
 });
 
-app.listen(5000, () => {
-  console.log("Server r unning on port 5000");
+const PORT =
+  process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(
+    `Server running on port ${PORT}`
+  );
 });
